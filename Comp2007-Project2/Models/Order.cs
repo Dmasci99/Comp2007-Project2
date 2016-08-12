301af7709cdca87d4e99514d997ed5750ac9e23b
Alex Friesen <friesen.alex95@gmail.com>
2016-08-12 16:38:35 -0400

    Trying to get the checkout page working. For some reason when we hit Submit on the AddressAndPayment page it catches an error and doesnt go through.

--- Comp2007-Project2/Models/Order.cs
+++ Comp2007-Project2/Models/Order.cs
@@ -1,24 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Comp2007_Project2.Models
 {
+    [Authorize]
+    [Bind(Exclude = "OrderId")]
     public class Order
     {
-        public virtual int OrderId { get; set; }
-        public virtual string Username { get; set; }
-        public virtual string FirstName { get; set; }
-        public virtual string LastName { get; set; }
-        public virtual string Address { get; set; }
-        public virtual string City { get; set; }
-        public virtual string PostalCode { get; set; }
-        public virtual string Country { get; set; }
-        public virtual string Phone { get; set; }
-        public virtual string Email { get; set; }
-        public virtual decimal Total { get; set; }
-        public virtual System.DateTime OrderDate { get; set; }
-        public virtual List<OrderDetail> OrderDetails { get; set; }
+        [ScaffoldColumn(false)]
+        public int OrderId { get; set; }
+        [ScaffoldColumn(false)]
+        public System.DateTime OrderDate { get; set; }
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
+        [StringLength(160)]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
+        [StringLength(160)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(70)]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(40)]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Province is required")]
+        [StringLength(40)]
+        public string Province { get; set; }
+        [Required(ErrorMessage = "Postal Code is required")]
+        [DisplayName("Postal Code")]
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(40)]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "Phone is required")]
+        [StringLength(24)]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Email Address is required")]
+        [DisplayName("Email Address")]
+
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [ScaffoldColumn(false)]
+        public decimal Total { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
     }
 }
