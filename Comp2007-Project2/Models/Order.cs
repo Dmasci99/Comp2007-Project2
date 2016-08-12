e4a5f197941cd1c3241765d88a346e93b130d3b1
KarenSpringford <jkspringford@gmail.com>
2016-08-11 22:03:01 -0400

    Added Order, OrderItem and OrderDetail classes.  Added the Orders and OrderDetails tables to StoreEntities.

---
+++ Comp2007-Project2/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comp2007_Project2.Models
+{
+    public class Order
+    {
+        public virtual int OrderId { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string City { get; set; }
+        public virtual string PostalCode { get; set; }
+        public virtual string Country { get; set; }
+        public virtual string Phone { get; set; }
+        public virtual string Email { get; set; }
+        public virtual decimal Total { get; set; }
+        public virtual System.DateTime OrderDate { get; set; }
+        public virtual List<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ Comp2007-Project2/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Comp2007_Project2.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public virtual int RecordId { get; set; }
+        public virtual string OrderItemId { get; set; }      
+        public virtual int Count { get; set; }
+        public virtual System.DateTime DateCreated { get; set; }
+        public virtual int GameId { get; set; }
+        public virtual Game Game { get; set; }
+
+    }
+}
