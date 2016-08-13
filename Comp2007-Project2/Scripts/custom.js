a30a1933cf5b5452c2f8f646f0f06c1c32732735
emdaisy1 <hilborn_emmav@live.ca>
2016-08-12 23:59:45 -0400

    Internal docs

--- Comp2007-Project2/Scripts/custom.js
+++ Comp2007-Project2/Scripts/custom.js
@@ -1,12 +1,13 @@
 ﻿/**
 * Authors : Emma Hilborn (200282755),
             Alex Friesen (200302342),
-            Dan Masci (200299037)
+            Dan Masci (200299037),
+            Karen Springford(200299681)
 * Class : Enterprise Computing
 * Semester : 4
 * Professor : Tom Tsiliopolous
 * Purpose : Final Team Project - E-Commerce Store
-* Website Name : EzGames1.azurewebsites.net
+* Website Name : EzGames2.azurewebsites.net
 */
 
 /**
